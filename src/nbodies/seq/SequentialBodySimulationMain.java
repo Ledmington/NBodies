569e4d0a08a50f1d709fdd1cde9919c862ab7ac3
Ledmington <68538713+Ledmington@users.noreply.github.com>
2022-03-15 12:02:06 +0100

    Added gitattributes

--- src/nbodies/seq/SequentialBodySimulationMain.java
+++ src/nbodies/seq/SequentialBodySimulationMain.java
@@ -2,6 +2,10 @@ package nbodies.seq;
 
 import nbodies.seq.view.SimulationView;
 
+import java.awt.*;
+
+import static java.lang.Integer.*;
+
 /**
  * Bodies simulation - legacy code: sequential, unstructured
  * 
@@ -10,8 +14,13 @@ import nbodies.seq.view.SimulationView;
 public class SequentialBodySimulationMain {
 
     public static void main(String[] args) {
+
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int) screenSize.getWidth();
+		int height = (int) screenSize.getHeight();
+		int size = min(width, height) - 30;
                 
-    	SimulationView viewer = new SimulationView(800, 800);
+    	SimulationView viewer = new SimulationView(size, size);
 
     	Simulator sim = new Simulator(viewer);
         sim.execute(50000);
